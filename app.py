68914150a1cd82924613421824a6d6254b6ae115
babara116 <lionseongheum@gmail.com>
2026-07-20 11:17:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 
-st.markdown("# AI 챗봇 만들기")
-st.markdown("---")
-st.markdown("## 질문을 하시면 AI 친구가 응답합니다.")
 st.header("1. 기본 정보 입력")
 user_id = st.text_input("이름", placeholder="이름")
 grade = st.radio("학년", ["1", "2", "3"], horizontal=True)
