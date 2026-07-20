0e7a15ffbeef80df386da8c708ebe5775b7ec17a
babara116 <lionseongheum@gmail.com>
2026-07-20 14:08:52 +0900

    Update 레이아웃.py

--- 레이아웃.py
+++ 레이아웃.py
@@ -5,7 +5,7 @@ def reset_all():
     st.session_state.weather = "맑음"
     st.session_state.top_type = "후드티"
     st.session_state.top_color = "밝음"
-    st.session_state.bottom_type = "청바지"
+    st.session_state.bottom_type = "슬랙스"
     st.session_state.bottom_color = "슬림"
     st.session_state.shoes = "스니커즈"
     st.session_state.acc = []
