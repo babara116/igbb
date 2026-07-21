3a85866be7ae0e7c7b6e64c74cca69ae17377b3c
babara116 <lionseongheum@gmail.com>
2026-07-21 10:16:09 +0900

    Update newlifeplaner.py

--- newlifeplaner.py
+++ newlifeplaner.py
@@ -78,7 +78,7 @@ def page_report():
             st.rerun()
 
 def page_ai_coach():
-    st.header(""AI코치와 대화하기")
+    st.header("AI코치와 대화하기")
     prompt = st.text_input("질문하기")
     if st.botton("보내기"):
         response = ai_client.responses.create(
